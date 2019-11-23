6e1329fa2a972ae9160b6986766e436e9569a64b
Kelvin Crawford <kcc16a@zcu.edu>
2019-11-23 15:36:31 -0600

    Update fix, added sound

--- app/src/main/java/com/example/criticalroll/MainActivity.java
+++ app/src/main/java/com/example/criticalroll/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.criticalroll;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+//
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Random;
 
@@ -18,7 +17,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageViewDice;
     private TextView critHit;
     private Random rng =  new Random();
-    private FloatingActionButton floatingActionButton;
+    MediaPlayer missSound;
+    MediaPlayer hitSound;
+    MediaPlayer diceSound;
+
 
 
     @Override
@@ -26,103 +28,124 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        critHit = findViewById(R.id.text_view_crit);
+        missSound = MediaPlayer.create(this, R.raw.no);
+        hitSound = MediaPlayer.create(this, R.raw.goofy);
+        diceSound = MediaPlayer.create(this, R.raw.dice3);
+
         imageViewDice = findViewById(R.id.image_view_dice);
         imageViewDice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 rollDice();
-
-                SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_prefs_key), Context.MODE_PRIVATE);
-
-                int defaultValue = getResources().getInteger(R.integer.saved_times_pressed_default_key);
-                int clicks = sharedPref.getInt(getString(R.string.saved_button_press_count_key), defaultValue);
-
-                clicks++;
-
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putInt("howMany", clicks);
-                editor.commit();
-
-
             }
 
-        });
-
-        floatingActionButton = findViewById(R.id.fab);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, CountActivity.class));
-            }
         });
     }
 
     private void rollDice(){
         int randomNumber = rng.nextInt(20) + 1;
 
-
         switch(randomNumber) {
             case 1:
                 imageViewDice.setImageResource(R.drawable.one);
-                critHit.setText(R.string.critical_h);
+                critHit.setText("CRITICAL MISS");
+                missSound.start();
                 break;
             case 2:
                 imageViewDice.setImageResource(R.drawable.two);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 3:
                 imageViewDice.setImageResource(R.drawable.three);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 4:
                 imageViewDice.setImageResource(R.drawable.four);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 5:
                 imageViewDice.setImageResource(R.drawable.five);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 6:
                 imageViewDice.setImageResource(R.drawable.six);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 7:
                 imageViewDice.setImageResource(R.drawable.seven);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 8:
                 imageViewDice.setImageResource(R.drawable.eight);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 9:
                 imageViewDice.setImageResource(R.drawable.nine);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 10:
                 imageViewDice.setImageResource(R.drawable.ten);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 11:
                 imageViewDice.setImageResource(R.drawable.eleven);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 12:
                 imageViewDice.setImageResource(R.drawable.twelve);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 13:
                 imageViewDice.setImageResource(R.drawable.thirteen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 14:
                 imageViewDice.setImageResource(R.drawable.fourteen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 15:
                 imageViewDice.setImageResource(R.drawable.fifthteen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 16:
                 imageViewDice.setImageResource(R.drawable.sixteen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 17:
                 imageViewDice.setImageResource(R.drawable.seventeen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 18:
                 imageViewDice.setImageResource(R.drawable.eightteen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 19:
                 imageViewDice.setImageResource(R.drawable.nineteen);
+                critHit.setText("");
+                diceSound.start();
                 break;
             case 20:
                 imageViewDice.setImageResource(R.drawable.twenty);
-                critHit.setText(R.string.critical_h);
+                critHit.setText("CRITICAL HIT");
+                hitSound.start();
                 break;
         }
     }
